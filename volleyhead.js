778cde2b9124857eeed19aa69f793ac382b32e57
powerzilly <ziliani.daniele@hotmail.it>
2014-03-17 14:02:18 +0100

    Aggiunte alcune funzioni per il controllo degli oggetti il disegno e la gravità

--- volleyhead.js
+++ volleyhead.js
@@ -95,6 +95,59 @@ function drawBall(){
 	contesto.drawImage(sprite2,this.sx,this.sy);
 }
 
+function drawPlayer(){
+	if(this.dir == 1){
+		if(this.pos > 9) this.pos = 0;
+		if(this.pos < 0) this.pos = 9;
+		startx = this.pos*150;
+		contesto.drawImage(sprite1,startx,180,150,170,this.sx,this.sy,60,80);
+		contesto.drawImage(face1,0,0,158,191,this.sx -10,this.sy -30,80,90);
+	}else{
+		if(this.pos > 9) this.pos = 0;
+		if(this.pos < 0) this.pos = 9;
+		startx = this.pos*150;
+		contesto.drawImage(sprite1,startx,0,150,170,this.sx,this.sy,60,80);
+		contesto.drawImage(face2,0,0,158,191,this.sx -10,this.sy -30,80,90);	
+	}
+}
+
+function drawBox(){
+	contesto.fillstyle = this.fillstyle;
+	contesto.fillRect(this.sx,this.sy,this.swidth,this.sheight);
+}
+
+function dinamicaPalla(){
+	//Movimento palla
+	myBall.sy -= myBall.dy;
+	myBall.sx += myBall.dx;
+	
+	//Urto terreno
+	if(myBall.sy >= canvasHeigth -50){
+		game.start = false;
+		myBall.dx = 0;
+		myBall.dy = 0;
+		myBall.sy = canvasHeight -200;
+		if(myBall.sx < (canvasWidth / 2)-20){
+			game.puntiPlayer1++;
+			myBall.sx = (canvasWidth / 2) +150;
+		}else{
+			game.puntiPlayer2++;
+			myBall.sx = (canvasWidth / 2) -150;
+		}
+		document.getElementById("Player1Score").innerHTML = game.puntiPLayer1;
+		document.getElementById("Player2Score").innerHTML = game.puntiPLayer2;
+		playSound(win);
+	}
+	
+	//Gravità
+	if(game.start) myBall.dy -= 0.25;
+	
+	//Attrito laterale
+	if(myBall.dx > 0) myBall.dx -= 0.03;
+	if(myBall.dx < 0) myBall.dx += 0.03;	
+}
+
+
 
 var canvas = document.getElementById("canvasvolley");
 
